cdc8fbbe3f9fc7053f10a9472da0604083a6414e
Ilya Zemskov <zemskov.ilya@gmail.com>
2023-03-10 18:35:39 -0300

    Unwind segue setup

--- Randomizer/ViewController.swift
+++ Randomizer/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet var minimumValueLabel: UILabel!
     @IBOutlet var maximumValueLabel: UILabel!
@@ -20,11 +20,23 @@ class ViewController: UIViewController {
         getRandomNumberButton.layer.cornerRadius = 10
     }
     
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else { return }
+        settingsVC.minimumValue = minimumValueLabel.text
+        settingsVC.maximumValue = maximumValueLabel.text
+    }
+    
     @IBAction func getRandomButtonWasTappad() {
         let minimumNumber = Int(minimumValueLabel.text ?? "") ?? 0
         let maximumNumber = Int(maximumValueLabel.text ?? "") ?? 100
 
         randomValueLabel.text = Int.random(in: minimumNumber...maximumNumber).formatted()
+    }
+    
+    @IBAction func unwind(for segue: UIStoryboardSegue) {
+        guard let settingsVC = segue.source as? SettingsViewController else { return }
+        minimumValueLabel.text = settingsVC.minimumValueTF.text
+        maximumValueLabel.text = settingsVC.maximumValueTF.text
     }
 }
 
